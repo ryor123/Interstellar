cea55aae2c7fdff39942b6ac27254e444dbdb027
ryor123 <166879578+ryor123@users.noreply.github.com>
2024-04-12 22:45:00 -0400

    Pass Changes (HOPEFULLY)

--- config.js
+++ config.js
@@ -2,8 +2,9 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: 'password',
-    theindian: 'ROckMAkEN',
+    permpass: 'ROckMAkEN',
+    daypass: '20240412',
+    
   },
   routes: true, // Change this to false if you just want to host a bare server.
   local: true, // Change this to false to disable local assets.
