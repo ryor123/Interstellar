1b94d8e4d09ad2feb3f67f644730dfa9e0877506
ryor123 <166879578+ryor123@users.noreply.github.com>
2024-04-12 20:14:03 -0400

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const config = {
   users: {
     // You can add multiple users by doing username: 'password'.
     interstellar: 'password',
-    theindiam: 'ROckMAkEN'
+    theindian: 'ROckMAkEN'
   },
   routes: true, // Change this to false if you just want to host a bare server.
   local: true, // Change this to false to disable local assets.
